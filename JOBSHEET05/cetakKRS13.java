53861bd06cd7e2f32cea71fd04392508d3d75a26
kinantinailah <kinanti.nailah06@gmail.com>
2025-10-08 09:46:58 +0700

    Percobaan 1

---
+++ JOBSHEET05/cetakKRS13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class cetakKRS13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false):");
+        boolean uktlunas = sc.nextBoolean();
+
+        if (uktlunas) {
+            System.out.println("Pembayaran UKT terverivikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+            
+            
+        }
+    }
+}
